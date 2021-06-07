75caab11679dd920ce618ea092f38a5e6dae8238
Quinton Fults <quinton.fults.work@gmail.com>
2021-06-07 13:16:51 -0700

    code

---
+++ db/users.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com'
+  },
+  {
+    name: 'Jane Doe',
+    email: 'janedoe@gmail.com'
+  }
+]
+
+module.exports = users
--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const { join } = require('path')
+const users = require('./db/users.js')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'))
+app.use(express.static(join(__dirname, 'public')))
+
+app.get('/users', (req, res) => {
+  res.json(users)
 })
 
-app.get('/movies', (req, res) => {
-  res.send('Goodfellas and The Room')
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'))
+// })
 
-app.get('/songs', (req, res) => {
-  res.send('Song 1 and Song 2')
-})
+// app.get('/contact', (req, res) => {
+//   res.sendFile(join(__dirname, 'contact.html'))
+// })
+
+// app.get('/movies', (req, res) => {
+//   res.send('Goodfellas and The Room')
+// })
+
+// app.get('/songs', (req, res) => {
+//   res.send('Song 1 and Song 2')
+// })
 
 // app.post('/movies', (req, res) => {
 
